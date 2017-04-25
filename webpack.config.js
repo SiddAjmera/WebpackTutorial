84ac8b9eaa994bb8c66d36ef8b28bcf35ce0e83a
Siddharth Ajmera <siddharth.ajmera@t-mobile.com>
2017-04-25 15:53:54 +0530

    Introduced transpiling features from ES6/JSX to Plain JS.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
     }
 }
